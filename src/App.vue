d0ddfab6fada176183de60333aeb8d4b28d28d68
Marco Aring <contato@marcoaring.com.br>
2021-05-24 18:24:29 -0300

    feat: change app styles

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <div id="nav">
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  </div> -->
+  <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  background-color: rgb(225, 225, 225);
+  margin: 0;
 }
 
-#nav {
-  padding: 30px;
+#app {
+  font-family: Roboto, sans-serif;
+  padding-top: 62px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  .main {
+    margin: 0 auto;
+    max-width: 800px;
+    width: 100%;
   }
 }
 </style>
