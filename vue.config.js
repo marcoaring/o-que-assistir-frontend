0b695d07e51bd8a62be2dac616c89a38ae80e500
Marco Aring <contato@marcoaring.com.br>
2021-05-24 18:23:49 -0300

    feat: change title of project

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable arrow-parens */
+module.exports = {
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'O que assistir?';
+      return args;
+    });
+  },
+};
