c033511f691c6920ab86c0c678f5e12355f440fb
Marco Aring <contato@marcoaring.com.br>
2021-05-27 18:45:00 -0300

    feat: remove vue-material

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import Vue from "vue";
-import VueMaterial from "vue-material";
-import "vue-material/dist/vue-material.min.css";
-import "vue-material/dist/theme/default.css";
 
 import App from "./App.vue";
 import "./registerServiceWorker";
@@ -9,8 +6,6 @@ import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
-
-Vue.use(VueMaterial);
 
 new Vue({
   router,
