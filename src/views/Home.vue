47382cdc48b11e38d6449958138eccdd1db843c9
Marco Aring <contato@marcoaring.com.br>
2021-05-31 20:07:01 -0300

    feat: insert filter in header component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -33,18 +33,15 @@
     </v-app-bar> -->
   <div class="home">
     <Header />
-    <Filters />
   </div>
 </template>
 
 <script>
 import Header from "@/components/header/Header.vue";
-import Filters from "@/components/filters/Filters.vue";
 
 export default {
   name: "Home",
   components: {
-    Filters,
     Header,
   },
 };
