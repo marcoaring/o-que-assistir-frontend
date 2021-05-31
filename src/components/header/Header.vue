d6d85368f72fb6051b2f9cece9af19946d957ca5
Marco Aring <contato@marcoaring.com.br>
2021-05-31 20:07:56 -0300

    feat: create header style

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="header">
-    <h1 class="title">O que assistir?</h1>
+  <div>
+    <v-toolbar color="cyan darken-1" dark>
+      <v-toolbar-title class="text-uppercase">O que assistir?</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn v-on:click="toggleFilters" icon>
+        <v-icon>mdi-filter</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <Filters v-show="showFilters" />
   </div>
 </template>
 
 <script>
+import Filters from "@/components/filters/Filters.vue";
+
 export default {
   name: "Header",
+  components: {
+    Filters,
+  },
+  data() {
+    return {
+      showFilters: false,
+    };
+  },
+  methods: {
+    toggleFilters() {
+      this.showFilters = !this.showFilters;
+    },
+  },
 };
 </script>
-
-<style scoped lang="scss">
-@import "./Header.scss";
-</style>
