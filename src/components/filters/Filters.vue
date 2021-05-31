196db36c44c00c52cf77d6cf33209dc18f32ffe4
Marco Aring <contato@marcoaring.com.br>
2021-05-31 20:08:18 -0300

    feat: create new filters

--- src/components/filters/Filters.vue
+++ src/components/filters/Filters.vue
@@ -1,62 +1,64 @@
 <template>
-  <div class="filters">
-    <section class="main">
-      <div class="filters__header header" v-on:click="toggleFilters">
-        <span class="header__title">Filtros:</span>
-        <img
-          alt="Ícone de Filtragem"
-          class="header__icon"
-          src="../../assets/icon-filter.svg"
-        />
-      </div>
-      <form class="form" v-if="showFilters">
-        <div class="form__field field">
-          <label for="author" class="field__label">Ator ou Atriz:</label>
-          <input
-            class="field__input"
-            name="author"
-            placeholder="Digite o nome de ator ou atriz"
-            v-model="author"
-          />
-        </div>
+  <div class="filters pa-4 pt-0">
+    <form @submit.prevent="submitFilters">
+      <v-autocomplete
+        :disable-lookup="loadingAuthor"
+        :items="authors"
+        :loading="loadingAuthor"
+        :search-input.sync="search"
+        color="cyan darken-1"
+        item-text="name"
+        item-value="id"
+        label="Ator ou Atriz:"
+        no-data-text="Sem atores ou atrizes encontrados"
+        placeholder="Começe a digitar para procurar"
+        v-model="author"
+      ></v-autocomplete>
 
-        <div class="form__field field">
-          <label for="author" class="field__label">Ano:</label>
-          <select class="field__select" name="year" v-model="year">
-            <option
-              :key="year.value"
-              :selected="year.selected"
-              :value="year.value"
-              v-for="year in yearsOptions"
-            >
-              {{ year.label }}
-              >
-            </option>
-          </select>
-        </div>
-      </form>
-    </section>
+      <v-select
+        :items="yearsOptions"
+        color="cyan darken-1"
+        item-text="label"
+        item-value="value"
+        label="Ano:"
+        placeholder="Escolha o ano de lançamento"
+        v-model="year"
+      ></v-select>
+
+      <v-select
+        :items="genres"
+        :loading="loadingGenres"
+        color="cyan darken-1"
+        item-text="name"
+        item-value="id"
+        label="Gênero:"
+        placeholder="Escolha o gênero"
+        v-model="genre"
+      ></v-select>
+
+      <v-btn color="cyan darken-1" elevation="2" type="submit" block dark
+        >Filtrar</v-btn
+      >
+    </form>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Filters",
-  // props: {
-  //   msg: String,
-  // },
   data() {
     return {
-      author: "",
-      year: "",
-      showFilters: false,
-      yearsOptions: [
-        {
-          label: "Escolha o ano de lançamento",
-          selected: true,
-          value: "",
-        },
-      ],
+      author: null,
+      authors: [],
+      genre: null,
+      genres: [],
+      loadingAuthor: false,
+      loadingGenres: true,
+      search: null,
+      year: null,
+      yearsOptions: [],
     };
   },
   methods: {
@@ -73,12 +75,68 @@ export default {
         currentYear--;
       }
     },
-    toggleFilters() {
-      this.showFilters = !this.showFilters;
+
+    getGenres() {
+      const url = "http://localhost:3000/api/films/genres";
+
+      axios
+        .get(url, {
+          headers: {
+            Authorization: `Bearer ${localStorage.token}`,
+          },
+        })
+        .then((res) => {
+          const { genres } = res.data;
+          this.genres = genres;
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => {
+          this.loadingGenres = false;
+        });
+    },
+
+    async submitFilters() {
+      console.log("Author -> ", this.author);
+      console.log("Ano -> ", this.year);
+      console.log("Gênero -> ", this.genre);
     },
   },
   beforeMount() {
     this.generateYears();
+    this.getGenres();
+  },
+  watch: {
+    search(val) {
+      if (this.loadingAuthor) return;
+
+      if (val.length <= 3) return;
+
+      this.loadingAuthor = true;
+
+      const url = "http://localhost:3000/api/search/author";
+
+      axios
+        .get(url, {
+          params: {
+            author: val,
+          },
+          headers: {
+            Authorization: `Bearer ${localStorage.token}`,
+          },
+        })
+        .then((res) => {
+          const { authors } = res.data;
+          this.authors = authors;
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => {
+          this.loadingAuthor = false;
+        });
+    },
   },
 };
 </script>
