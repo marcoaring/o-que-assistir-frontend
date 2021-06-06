779abb9e99c069e2475a7963316b0ecfa2ac2490
Marco Aring <contato@marcoaring.com.br>
2021-06-06 13:32:35 -0300

    feat: insert rent streamings

--- src/components/movie-card/MovieCard.vue
+++ src/components/movie-card/MovieCard.vue
@@ -66,30 +66,96 @@
         "
       ></v-divider>
 
-      <v-card-actions class="justify-end">
-        <!-- <v-btn color="cyan darken-1" text>Saber Mais</v-btn> -->
+      <v-card-actions
+        v-if="
+          this.$store.state.movies[0].streamings &&
+          this.$store.state.movies[0].streamings.rent.length > 0
+        "
+      >
+        <div
+          class="
+            align-center
+            d-flex
+            font-weight-bold
+            mx-2
+            tag-note
+            text-uppercase
+          "
+        >
+          Alugue em:
+          <v-tooltip
+            top
+            v-for="streaming in this.$store.state.movies[0].streamings.rent"
+            :key="streaming.id"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-img
+                :src="streaming.logo"
+                width="30px"
+                class="mx-2 rounded-circle"
+                v-bind="attrs"
+                v-on="on"
+              >
+              </v-img>
+            </template>
+            <span>{{ streaming.name }}</span>
+          </v-tooltip>
+        </div>
+      </v-card-actions>
+
+      <v-divider
+        class="mx-4"
+        v-if="
+          this.$store.state.movies[0].streamings &&
+          this.$store.state.movies[0].streamings.flatrate.length > 0
+        "
+      ></v-divider>
+
+      <v-card-actions class="justify-space-between">
+        <v-btn color="cyan darken-1" text>Saber Mais</v-btn>
         <v-btn color="light-blue darken-4" text>Já assisti!</v-btn>
       </v-card-actions>
     </v-card>
+
+    <v-btn
+      :elevation="15"
+      block
+      class="my-7"
+      color="cyan darken-1"
+      dark
+      v-if="this.$store.state.selectedYear"
+      v-on:click="searchMovie"
+      >Buscar Outro Filme</v-btn
+    >
   </div>
 </template>
 
 <script>
 export default {
   name: "MovieCard",
+  methods: {
+    async searchMovie() {
+      this.$store.commit("searchMovie");
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
-.tag {
-  flex-direction: column;
-  position: absolute;
-  right: 10px;
-  top: 10px;
-  z-index: 1;
+.card {
+  max-width: 400px;
+  width: 100%;
 
-  &-note {
-    font-size: 10px;
+  .tag {
+    flex-direction: column;
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    z-index: 1;
+
+    &-note {
+      font-size: 10px;
+    }
   }
 }
 </style>
